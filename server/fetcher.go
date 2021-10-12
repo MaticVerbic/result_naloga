86e6ceeff62d678132235458d57b302af26608e8
Matic Verbič <matic.verbicit@gmail.com>
2021-10-12 23:49:25 +0200

    removed unnecessary cors headers

--- server/fetcher.go
+++ server/fetcher.go
@@ -132,7 +132,7 @@ func NewCORS() func(next http.Handler) http.Handler {
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
-		AllowedHeaders:   []string{"Authorization", "Content-Type", "X-Core-Api-Key", "X-Core-Accept-Currency", "X-Core-Accept-Language", "X-Accept-Locale", "X-Active-Role"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
 		AllowCredentials: true,
 	})
 	return cors.Handler
